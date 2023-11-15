19c4413326249cf06a97441740a84262c7b86c5a
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-14 23:19:03 -0800

    initial unit test passing

---
+++ src/LastChristmas.Core/Extensions/HtmlParsingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace LastChristmas.Core.Extensions
+{
+	public static class HtmlParsingExtensions
+	{
+        public static string AsHtmlString(this string value) =>
+            string.IsNullOrWhiteSpace(value) ?
+            "<html></html>" : value;
+
+        public static List<Ranking> AddNode(this List<Ranking> list,
+            HtmlNode node, Func<HtmlNode,Ranking> conversion)
+        {
+            list.Add(conversion(node));
+            return list;
+        }
+
+
+        public static string SelectText(this HtmlNode node, string selector)
+        {
+            HtmlNode result = node.SelectSingleNode(selector);
+            return (result == null) ? "Unknown" :
+                result.InnerText;
+        }
+
+        public static int SelectTextAsInt(this HtmlNode node,
+            string selector)
+        {
+            HtmlNode result = node.SelectSingleNode(selector);
+            return (Int32.TryParse(result.InnerText, out int value)) ?
+                value : 0;
+        }
+    }
+}
+
--- src/LastChristmas.Core/LastChristmasRankingResponse.cs
+++ src/LastChristmas.Core/LastChristmasRankingResponse.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using LastChristmas.Core.Extensions;
+
 namespace LastChristmas.Core
 {
     /// <summary>
@@ -6,12 +10,20 @@ namespace LastChristmas.Core
     /// </summary>
     public class LastChristmasRankingResponse 
 	{
-		private string _httpResponse = string.Empty;
+        private const string _chartXpath =
+            "/html/body/div/div/div/main/div[2]/table/tbody/tr";
 
-		internal LastChristmasRankingResponse(string httpResponse) {
-			_httpResponse = string.IsNullOrWhiteSpace(httpResponse) ?
-				string.Empty : _httpResponse;
+        private const string _titlePath = "td[2]";
+        private const string _valuePath = "td[7]";
+
+        private HtmlDocument _document;
+
+		internal LastChristmasRankingResponse(string response) {
+
+            _document = new HtmlDocument();
+            _document.LoadHtml(response.AsHtmlString());
 		}
+
 
         /// <summary>
         /// Gets the rankings associated with the Last Christmas response.
@@ -22,7 +34,29 @@ namespace LastChristmas.Core
         /// spontaneous singing and festive dance moves.
         /// </returns>
         public IEnumerable<Ranking> Rankings =>
-			Enumerable.Empty<Ranking>();
-	}
+            _document.DocumentNode.
+                SelectNodes(_chartXpath).Aggregate(new List<Ranking>(),
+                (rankings, node) => rankings.AddNode(node,
+                    node => GetRankingFor(node)));
+
+
+        private Ranking GetRankingFor(HtmlNode node) =>
+            new Ranking(GetText(node), GetRank(node));
+
+
+        private string GetText(HtmlNode node)
+        {
+
+            string title = node.SelectText(_titlePath);
+            return title.Replace("Singles Top", "").
+                Trim().
+                Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                FirstOrDefault("Unknown");
+        }
+
+
+        private int GetRank(HtmlNode node) =>
+            node.SelectTextAsInt(_valuePath);
+    }
 }
 
