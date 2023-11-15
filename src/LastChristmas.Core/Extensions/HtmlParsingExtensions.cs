478a6ef693dcc01e1e49766538653c6e19c504f8
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-14 23:27:06 -0800

    added summary comments

--- src/LastChristmas.Core/Extensions/HtmlParsingExtensions.cs
+++ src/LastChristmas.Core/Extensions/HtmlParsingExtensions.cs
@@ -3,12 +3,32 @@ using HtmlAgilityPack;
 
 namespace LastChristmas.Core.Extensions
 {
-	public static class HtmlParsingExtensions
+    /// <summary>
+    /// Provides extension methods for parsing and manipulating HTML content.
+    /// </summary>
+    public static class HtmlParsingExtensions
 	{
+        /// <summary>
+        /// Converts a string to an HTML-formatted string, wrapping it
+        /// with an HTML tag if the input is empty or whitespace.
+        /// </summary>
+        /// <param name="value">The input string to be converted.</param>
+        /// <returns>An HTML-formatted string.</returns>
         public static string AsHtmlString(this string value) =>
             string.IsNullOrWhiteSpace(value) ?
             "<html></html>" : value;
 
+        /// <summary>
+        /// Adds a new Ranking object to the list based on the
+        /// provided HTML node and conversion function.
+        /// </summary>
+        /// <param name="list">The list of rankings to which the
+        /// new ranking is added.</param>
+        /// <param name="node">The HTML node containing data for the
+        /// new ranking.</param>
+        /// <param name="conversion">The conversion function to
+        /// transform the HTML node into a Ranking object.</param>
+        /// <returns>The updated list of rankings.</returns>
         public static List<Ranking> AddNode(this List<Ranking> list,
             HtmlNode node, Func<HtmlNode,Ranking> conversion)
         {
@@ -16,7 +36,15 @@ namespace LastChristmas.Core.Extensions
             return list;
         }
 
-
+        /// <summary>
+        /// Selects and returns the inner text of an HTML node based on
+        /// the provided CSS selector.
+        /// </summary>
+        /// <param name="node">The HTML node from which to select text.</param>
+        /// <param name="selector">The CSS selector used to locate
+        /// the desired node.</param>
+        /// <returns>The inner text of the selected HTML node or
+        /// "Unknown" if the node is not found.</returns>
         public static string SelectText(this HtmlNode node, string selector)
         {
             HtmlNode result = node.SelectSingleNode(selector);
@@ -24,6 +52,16 @@ namespace LastChristmas.Core.Extensions
                 result.InnerText;
         }
 
+        /// <summary>
+        /// Selects and returns the inner text of an HTML node as an
+        /// integer based on the provided CSS selector.
+        /// </summary>
+        /// <param name="node">The HTML node from which to
+        /// select text as an integer.</param>
+        /// <param name="selector">The CSS selector used to
+        /// locate the desired node.</param>
+        /// <returns>The inner text of the selected HTML node parsed as an
+        /// integer or 0 if parsing fails.</returns>
         public static int SelectTextAsInt(this HtmlNode node,
             string selector)
         {
--- src/LastChristmas.Core/Extensions/HttpClientExtensions.cs
+++ src/LastChristmas.Core/Extensions/HttpClientExtensions.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 namespace LastChristmas.Core.Extensions
 {
-	public static class HttpClientExtensions
+    /// <summary>
+    /// Provides extension methods for interacting with an HttpClient to
+    /// retrieve Last Christmas rankings.
+    /// </summary>
+    public static class HttpClientExtensions
 	{
-		public const string LastChristmasStatsUrl =
+        /// <summary>
+        /// The URL for retrieving Last Christmas statistics.
+        /// </summary>
+        public const string LastChristmasStatsUrl =
 			"https://acharts.co/song/3018";
 
-		public static Task<LastChristmasRankingResponse>
+        /// <summary>
+        /// Asynchronously retrieves Last Christmas rankings using the
+        /// provided HttpClient.
+        /// </summary>
+        /// <param name="client">The HttpClient used to make
+        /// the request.</param>
+        /// <returns>A task representing the asynchronous operation that,
+        /// upon completion, returns the
+        /// LastChristmasRankingResponse.</returns>
+        public static Task<LastChristmasRankingResponse>
 			GetLastChristmasRankingsAsync(this HttpClient client)
 		{
 			return client.GetStringAsync(LastChristmasStatsUrl).
