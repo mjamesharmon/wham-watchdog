a73c8e4ab7332c9040a8f9f7c0c4cdb453bf5ee4
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-15 19:15:26 -0800

    removed LastChristmas.Console.RankingExtensions.cs

--- src/LastChristmas.Console/Program.cs
+++ src/LastChristmas.Console/Program.cs
@@ -1,6 +1,5 @@
 ﻿using LastChristmas.Core;
 using LastChristmas.Core.Extensions;
-using LastChristmas.Console;
 
 HttpClient http = new HttpClient();
 string response = await http.GetLastChristmasRankingsAsync().
