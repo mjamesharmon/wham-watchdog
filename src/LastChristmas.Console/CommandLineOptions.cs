49d8a903f5d4f8151e236e2bdf8fab5413572059
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-17 08:54:13 -0800

    setup configuration

--- src/LastChristmas.Console/CommandLineOptions.cs
+++ src/LastChristmas.Console/CommandLineOptions.cs
@@ -6,11 +6,28 @@ namespace LastChristmas.Console
 	public class CommandLineOptions
 	{
 		[Value(index: 0, Required = true, HelpText = "Path to output file")]
-		public string OutputFile { get; set; } = string.Empty;
+		public string OutputPath { get; set; } = string.Empty;
 
-		[Option(shortName: 'x', longName: "xslt", HelpText = "Path to xslt",
+		[Option(shortName: 't', longName: "transform", HelpText = "Path to xslt",
 			Required = false, Default = "")]
 		public string Xslt { get; set; } = string.Empty;
+
+		[Option(shortName: 'o', longName: "output",
+			HelpText = "filename for output produced by the -t option",
+			Default = "")]
+		public string OutputFile { get; set; } = string.Empty;
+
+		[Option(longName: "md", Required = false,
+			HelpText = "inclide markdown transform")]
+		public bool IncludeMarkdown { get; set; } = false;
+
+		[Option(longName: "html", Required = false,
+			HelpText = "Include html transform")]
+		public bool IncludeHtml { get; set; } = false;
+
+		[Option(longName: "xml", Required = false,
+			HelpText = "include raw xml")]
+		public bool IncludeXml { get; set; } = false;
 	}
 }
 
---
+++ src/LastChristmas.Console/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LastChristmas.Console
+{
+	public class Configuration
+	{
+		public Configuration()
+		{
+		}
+	}
+}
+
