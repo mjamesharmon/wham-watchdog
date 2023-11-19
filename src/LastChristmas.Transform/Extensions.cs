c5f42e48e70b336d01e553297df4d51598741b4e
mjamesharmon <m.james.harmon@fastmail.com>
2023-11-18 16:15:34 -0800

    added support for loading xslt transforms from the assembly

---
+++ src/LastChristmas.Transform/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace LastChristmas.Transform
+{
+	public static class Extensions
+	{
+        private static readonly string MarkdownTransform =
+            "LastChristmas.Transform.md_transform.xsl";
+
+        private static readonly string HtmlTransform =
+            "LastChristmas.Transform.html_transform.xsl";
+
+        public static string LoadMarkdownTransform(this Assembly assembly) {
+            return assembly.LoadFromStream(MarkdownTransform);
+        }
+
+        public static string LoadHtmlTransform(this Assembly assembly) {
+            return assembly.LoadFromStream(HtmlTransform);
+        }
+
+        private static string LoadFromStream(this Assembly assembly,
+            string resourceName)
+        {
+            using (Stream? stream = assembly.
+                GetManifestResourceStream(resourceName))
+            using (StreamReader reader = stream.AsStreamReader())
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private static StreamReader AsStreamReader(this Stream? stream) =>
+            new StreamReader(stream ?? throw new InvalidOperationException());
+	}
+}
+
